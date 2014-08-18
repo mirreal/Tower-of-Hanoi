c83b8473e379d98c3770865c8aac3da2716e544e
mirreal <zgkm181142521@gmail.com>
2014-08-18 20:45:34 +0800

    加入一点交互元素，以及代码优化

--- hanoi.js
+++ hanoi.js
@@ -1,8 +1,152 @@
+function Game(startDisk) {
+  this.startDisk = startDisk;
+  this.towers = [];
+  this.solution = [];
+  this.checkedTower = null;
+  this.isWatched = false;
+
+  this.canvas = new Canvas();
+  this.eventHandler = new EventHandler();
+
+  this.eventHandler.addHandler('move', this.move.bind(this));
+  this.eventHandler.addHandler('up', this.cursorUp.bind(this));
+  this.eventHandler.addHandler('down', this.cursorDown.bind(this));
+  this.eventHandler.addHandler('play', this.play.bind(this));
+  this.eventHandler.addHandler('watch', this.watch.bind(this));
+
+  this.init();
+}
+
+Game.prototype.init = function() {
+  var towers = this.towers;
+
+  towers[0] = new Tower(100, this.startDisk);
+  towers[1] = new Tower(300);
+  towers[2] = new Tower(500);
+  // 确定三个塔的位置关系
+  towers.forEach(function(tower, index) {
+    tower.left = towers[(index+2) % 3];
+    tower.right = towers[(index+1) % 3]
+  });
+  this.checkedTower = towers[0];
+  this.canvas.cursor = towers[0];
+  this.canvas.cursorShow = false;
+
+  this.draw();
+};
+
+Game.prototype.play = function(level) {
+  this.startDisk = level;
+  this.isWatched = false;
+
+  this.init();
+};
+
+
+Game.prototype.watch = function(level) {
+  this.startDisk = level;
+  this.solution = [];
+  this.isWatched = true;
+
+  this.init();
+  this.solutionShow();
+};
+
+Game.prototype.succuss = function() {
+  if (this.towers[2].disks.length == this.startDisk) {
+    this.isWatched = true;
+    alert('You won!');
+  }
+};
+
+Game.prototype.draw = function() {
+  var self = this;
+
+  this.canvas.drawGrid("lightgrey", 20, 20);
+  this.towers.forEach(function(tower) {
+    self.canvas.drawDisk(tower);
+  });
+
+  if (!this.isWatched) this.succuss();
+};
+
+Game.prototype.move = function(data) {
+  // data: left - move left, right - move right
+  if (this.isWatched) return;
+  if (this.checkedTower) {
+    var disks = this.checkedTower.disks;
+    var disk = disks[disks.length-1];
+    if (!disk) return;
+    var moveToDisks = this.checkedTower[data].disks;
+    if (moveToDisks.length !== 0) {
+      if (moveToDisks[moveToDisks.length-1].value < disk.value){
+        return;
+      }
+    }
+    disk = this.checkedTower.disks.pop();
+    this.checkedTower[data].disks.push(disk);
+    this.canvas.cursor = this.checkedTower;
+    this.checkedTower = this.checkedTower[data];
+  }
+  this.canvas.cursor = this.canvas.cursor[data];
+  this.draw();
+}
+
+Game.prototype.cursorUp = function() {
+  if (this.isWatched) return;
+  this.checkedTower = null;
+  this.canvas.cursorShow = true;
+  this.draw();
+}
+
+Game.prototype.cursorDown = function() {
+  if (this.isWatched) return;
+  if (this.canvas.cursor.disks.length !== 0) {
+    this.checkedTower = this.canvas.cursor;
+    this.canvas.cursorShow = false;
+    this.draw();
+  }
+}
+
+Game.prototype.solutionShow = function() {
+  var self = this;
+
+  var towers = this.towers;
+  this.solve(this.startDisk, towers[0], towers[1], towers[2]);
+
+  var solution = this.solution;
+  var n = solution.length;
+  // 使用递归函数实现异步API(setTimeout)的顺序执行
+  function tryNext(i) {
+    if (i >= n) return;
+    console.log(i);
+    setTimeout(function() {
+      self.moveAuto(solution[i].from, solution[i].to);
+      self.draw();
+      tryNext(i+1);
+    }, 600);
+  }
+  tryNext(0);
+};
+
+Game.prototype.solve = function(n, a, b, c) {
+  if (n == 1) return this.solution.push({from: a, to: c});
+  this.solve(n-1, a, c, b);
+  this.solution.push({from: a, to: c});
+  this.solve(n-1, b, a, c);
+};
+
+Game.prototype.moveAuto = function(from, to) {
+  var disk = from.disks.pop();
+  to.disks.push(disk);
+};
+
+
 function Canvas() {
-  this.canvas = document.getElementById("canvas");
-  this.context = canvas.getContext("2d");
-  this.colors = ["#C03", "yellow", "green", "orange", "brown",
-      "Magenta","DeepSkyBlue"];
+  this.canvas = document.getElementById('canvas');
+  this.context = canvas.getContext('2d');
+  this.colors = ['#C03', 'yellow', 'green', 'orange', 'brown',
+      'Magenta','DeepSkyBlue', 'pink', 'DarkKhaki'];
 
   this.cursorShow = false;
   this.cursor = null;
@@ -72,15 +216,13 @@ function EventHandler() {
   this.listen();
 }
 
-EventHandler.prototype.on = function(event, callback) {
-  //if (!this.events[event]) this.events[event] = [];
-  //this.events[event].push(callback);
+EventHandler.prototype.addHandler = function(event, callback) {
   this.events[event] = callback;
 }
 
-EventHandler.prototype.emit = function(event) {
+EventHandler.prototype.handle = function(event, data) {
   var callback = this.events[event];
-  if (callback) callback();
+  if (callback) callback(data);
 }
 
 EventHandler.prototype.listen = function() {
@@ -91,177 +233,36 @@ EventHandler.prototype.listen = function() {
     switch (event.keyCode) {
       case 37:
       case 65:
-        self.emit('left');
+        self.handle('move', 'left');
         break;
       case 38:
       case 87:
-        self.emit('up');
+        self.handle('up');
         break;
       case 39:
       case 68:
-        self.emit('right');
+        self.handle('move', 'right');
         break;
       case 40:
       case 83:
-        self.emit('down');
+        self.handle('down');
         break;
     }
   });
-}
 
-function Game(startDisk) {
-  this.startDisk = startDisk;
-  this.towers = [];
-  this.solves = [];
-  
-  this.canvas = new Canvas();
-  this.eventHandler = new EventHandler();
+  var play = document.getElementById('play-button');
+  play.addEventListener('click', function(event) {
+    event.preventDefault();
+    var level = document.getElementById('level').value;
+    self.handle('play', level);
+  }, false);
 
-  this.eventHandler.on('left', this.moveLeft.bind(this));
-  this.eventHandler.on('right', this.moveRight.bind(this));
-  this.eventHandler.on('up', this.cursorUp.bind(this));
-  this.eventHandler.on('down', this.cursorDown.bind(this));
-
-  this.init();
-  
-
-  this.start();
-}
-
-Game.prototype.init = function() {
-  var towerA = new Tower(100, this.startDisk, true);
-  var towerB = new Tower(300);
-  var towerC = new Tower(500);
-  towerA.left  = towerC;
-  towerA.right = towerB;
-  towerB.left  = towerA;
-  towerB.right = towerC;
-  towerC.left  = towerB;
-  towerC.right = towerA;
-  this.towers  = [towerA, towerB, towerC];
-  this.canvas.cursor = this.towers[0];
-  this.draw();
-};
-
-Game.prototype.start = function() {
-  var self = this;
-  //this.solveShow();
-};
-
-Game.prototype.succuss = function() {
-  if (this.towers[2].disks.length == this.startDisk) {
-    alert('You won!');
-  }
-}
-
-Game.prototype.moveLeft = function() {
-  var self = this;
-
-  for (var i = self.towers.length-1; i >= 0; i--) {
-    if (self.towers[i].checked == true) {
-      checkedTower = self.towers[i];
-      var disk = checkedTower.disks[checkedTower.disks.length-1]
-      if (!disk) return;
-      var leftDisks = checkedTower.left.disks;
-      if (leftDisks.length !== 0) {
-        if (leftDisks[leftDisks.length-1].value < disk.value){
-          return;
-        }
-      }
-      disk = checkedTower.disks.pop();
-      checkedTower.left.disks.push(disk);
-      checkedTower.checked = false;
-      checkedTower.left.checked = true;
-      this.canvas.cursor = self.towers[i];
-      
-      break;
-    }
-  }
-  if (this.canvas.cursor) this.canvas.cursor = this.canvas.cursor.left;
-  this.draw();
-}
-Game.prototype.moveRight = function() {
-  var self = this;
-
-  for (var i = self.towers.length-1; i >= 0; i--) {
-    if (self.towers[i].checked == true) {
-      checkedTower = self.towers[i];
-      var disk = checkedTower.disks[checkedTower.disks.length-1]
-      if (!disk) return;
-      var rightDisks = checkedTower.right.disks;
-      if (rightDisks.length !== 0) {
-        if (rightDisks[rightDisks.length-1].value < disk.value){
-          return;
-        }
-      }
-      disk = checkedTower.disks.pop();
-      checkedTower.right.disks.push(disk);
-      checkedTower.checked = false;
-      checkedTower.right.checked = true;
-      this.canvas.cursor = self.towers[i];
-      break;
-    }
-  }
-  if (this.canvas.cursor) this.canvas.cursor = this.canvas.cursor.right;
-  this.draw();
-}
-Game.prototype.cursorUp = function() {
-  var self = this;
-
-  self.towers.forEach(function(tower) {
-    tower.checked = false;
-  });
-  this.canvas.cursorShow = true;
-  this.draw();
-}
-Game.prototype.cursorDown = function() {
-  var self = this;
-
-  if (this.canvas.cursor) this.canvas.cursor.checked = true;
-  this.canvas.cursorShow = false;
-  this.draw();
-}
-
-
-Game.prototype.solveShow = function() {
-  var self = this;
-
-  var towers = this.towers;
-  this.solve(this.startDisk, towers[0], towers[1], towers[2]);
-
-  var solves = this.solves;
-  var n = solves.length;
-  function tryNext(i) {
-    if (i >= n) return;
-    setTimeout(function() {
-      self.move(solves[i].from, solves[i].to);
-      self.draw();
-      tryNext(i+1);
-    }, 600);
-  }
-  tryNext(0);
-};
-
-Game.prototype.solve = function(n, a, b, c) {
-  if (n == 1) return this.solves.push({from: a, to: c});
-  this.solve(n-1, a, c, b);
-  this.solves.push({from: a, to: c});
-  this.solve(n-1, b, a, c);
-};
-
-Game.prototype.move = function(from, to) {
-  var disk = from.disks.pop();
-  to.disks.push(disk);
-};
-
-Game.prototype.draw = function() {
-  var self = this;
-
-  this.canvas.drawGrid("lightgrey", 20, 20);
-  this.towers.forEach(function(tower) {
-    self.canvas.drawDisk(tower);
-  });
-  this.succuss();
+  var watch = document.getElementById('watch-button');
+  watch.addEventListener('click', function(event) {
+    event.preventDefault();
+    var level = document.getElementById('level').value;
+    self.handle('watch', level);
+  }, false);
 };
 
 
@@ -271,10 +272,9 @@ function Disk(value, color) {
 }
 
 
-function Tower(position, diskNumber, checked) {
+function Tower(position, diskNumber) {
   this.position = position;
   this.diskNumber = diskNumber;
-  this.checked = checked;
 
   this.disks = [];
   this.addDisks();
